c2bdbb31ef7f61572a2972e1e9f450e9f971eed3
akosijose <josegeraldlumbao0307@gmail.com>
2022-08-30 15:48:47 +0800

    update

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -5,47 +5,44 @@ import Moment from "moment";
 const List = () => {
   const [list, fetchList] = useState([]);
 
-  const getData = () => {
-    fetch("https://api.spacexdata.com/v4/launches")
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        fetchList(res);
-      });
-  };
-
   useEffect(() => {
-    getData();
+    setTimeout(async () => {
+      const res = await fetch("https://api.spacexdata.com/v4/launches");
+      const data = await res.json();
+      fetchList(data);
+    }, 3000);
   }, []);
 
   return (
     <div>
       <main>
         {list.map((value) => {
-          {/* let formatDate = Moment(value.date_local).format("DD-MM-YYYY"); */}
-
           return (
-            <div className="container" key={value.id}>
-              <div className="image">
-                <img src="" alt="image" />
-              </div>
-              <div className="text">
-                <div className="header">
-                  <h3>
-                    {value.flight_number}: {value.name} ({(value.date_local = Moment().format("DD-MM-YYYY"))})
-                  </h3>
+            list && (
+              <div className="container" key={value.id}>
+                <div className="image">
+                  <img src={`${value.links.patch.large}.png`} alt="image" />
                 </div>
-                <div className="details">
-                  <p>
-                    <span>Details: </span>
-                    {value.details}
-                  </p>
+                <div className="text">
+                  <div className="header">
+                    <h3>
+                      {value.flight_number}: {value.name} (
+                      {Moment(value.date_local).format("DD-MM-YYYY")})
+                    </h3>
+                  </div>
+                  <div className="details">
+                    <p>
+                      <span>Details: </span>
+                      {value.details}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            )
           );
         })}
       </main>
+      {!list && <div>loading...</div>}
     </div>
   );
 };
